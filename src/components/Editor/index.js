710900adb624b77f96b10b29a3ee2adb7168a81d
姚林 <yaolin1@xiaomi.com>
2019-08-26 19:59:47 +0800

    基本样式

--- src/components/Editor/index.js
+++ src/components/Editor/index.js
@@ -17,11 +17,20 @@ class MyEditor extends React.Component {
         RichUtils.toggleInlineStyle(this.state.editorState, style)
       );
   }
+  toggleBlockType(type){
+    this.onChange(
+      RichUtils.toggleBlockType(this.state.editorState, type)
+    );
+  }
   render() {
     return (
         <div>
-            <button onClick={() => this.toggleInlineStyle('BOLD')}>bold</button>  
-            <button onClick={() => this.toggleInlineStyle('RED')}>red</button>        
+            <button onClick={() => this.toggleBlockType('header-three')}>标题</button>  
+            <button onClick={() => this.toggleInlineStyle('BOLD')}>加粗</button>  
+            <button onClick={() => this.toggleInlineStyle('ITALIC')}>斜体</button> 
+            <button onClick={() => this.toggleInlineStyle('UNDERLINE')}>下划线</button>    
+            <button onClick={() => this.toggleBlockType('unordered-list-item')}>无序列表</button>  
+            <button onClick={() => this.toggleBlockType('ordered-list-item')}>有序列表</button>  
             <Editor editorState={this.state.editorState} onChange={this.onChange} customStyleMap={styleMap}/>
         </div>
 
